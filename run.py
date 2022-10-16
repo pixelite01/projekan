0e5c24d908b4fc4cc8df8969d2759cf1cf8abf3f
pixelite01 <108801292+pixelite01@users.noreply.github.com>
2022-10-16 21:11:04 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #######################################################
-# Name           : Brute Facebook (BF)                #
+# Name           : Yayan Multi Brute Facebook (YMBF)  #
 # File           : run.py                             #
 # Author         : Moch Yayan Juan Alvredo XD.        #
 # Github         : https://github.com/Yayan-XD        #
@@ -30,13 +30,9 @@ except ImportError:
     print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
     os.system('pip install rich')
 #################################################################################
-from src.cok import Brute
+import os
+from src import cok
 
 if __name__ == '__main__':
-    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
-   # os.system("clear")
-   # print("please wait....")
-   # print()
-   # print("script sedang dalam perbaikan..")
-   # exit()
-    Brute()
+    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
+    cok.Brute().moch_yayan()

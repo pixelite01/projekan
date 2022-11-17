2e43c45ec5b48fdbdc1813405c744d7ede2c31b0
pixelite01 <108801292+pixelite01@users.noreply.github.com>
2022-11-18 01:41:08 +0700

    Add files via upload

--- run.py
+++ run.py
@@ -5,7 +5,7 @@
 # Github         : https://github.com/Yayan-XD        #
 # Facebook       : https://www.facebook.com/KM39453   #
 # Website        : https://www.yayanxd.my.id          #
-# Python version : 0.4                                #
+# Python version : 3.11                               #
 #######################################################
 
 ############# DON'T REMOVE THIS FUNCTIONS #############
@@ -30,8 +30,13 @@ except ImportError:
     print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
     os.system('pip install rich')
 #################################################################################
-from src import cok
+from src.cok import Brute
 
 if __name__ == '__main__':
-    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...")
-    cok.Brute()
+    os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
+   # os.system("clear")
+   # print("please wait....")
+   # print()
+   # print("script sedang dalam perbaikan..")
+   # exit()
+    Brute()

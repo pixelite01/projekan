b5a39de96ef883e7783b1790302240c37464be22
pixelite01 <108801292+pixelite01@users.noreply.github.com>
2022-11-18 01:42:28 +0700

    Add files via upload

--- insta/dump/logo1.py
+++ insta/dump/logo1.py
@@ -10,10 +10,11 @@
 
 ############# DON'T REMOVE THIS FUNCTIONS #############
 
-import os, sys, random
+import sys, os, datetime
 
-#----------- MODULE RICH -------------
+#---- MODULE RICH IN PYTHON -------
 from rich import print as prints
+from datetime import datetime
 from rich.panel import Panel
 
 reed = "[bold red]"
@@ -31,12 +32,19 @@ class Turu:
         else:
             try:os.system("clear")
             except:pass
-        WAR = random.choice(["[deep_pink3]","[green]","[cyan]","[blue]"])
-        prints(Panel(f"""{WAR}  
-    ██████╗██╗  ██╗██╗███╗   ██╗██████╗  █████╗ 
-   ██╔════╝██║  ██║██║████╗  ██║██╔══██╗██╔══██╗
-   ██║     ███████║██║██╔██╗ ██║██║  ██║███████║
-   ██║     ██╔══██║██║██║╚██╗██║██║  ██║██╔══██║
-   ╚██████╗██║  ██║██║██║ ╚████║██████╔╝██║  ██║
-    ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ ╚═╝  ╚═╝
- {WAR}version: 0.4[/]"""))
+        prints(Panel(f"""{blue} ____             _         ______                  {reed} _____ _____ 
+{blue}|  _ \           | |       |  ____|                 {reed}|_   _/ ____|
+{blue}| |_) |_ __ _   _| |_ ___  | |__ ___  _ __ ___ ___  {reed}  | || |  __ 
+{blue}|  _ <| '__| | | | __/ _ \ |  __/ _ \| '__/ __/ _ \ {reed}  | || | |_ |
+{blue}| |_) | |  | |_| | ||  __/ | | | (_) | | | (_|  __/ {reed} _| || |__| |
+{blue}|____/|_|   \__,_|\__\___| |_|  \___/|_|  \___\___| {reed}|_____\_____|
+                        {reed}BY Yayan XD. @2022""", style="bold white", width=70, title=f"[cyan]{self.waktu()}"))
+
+    def waktu(self):
+        now = datetime.now()
+        hours = now.hour
+        if 4 <= hours < 12:timenow = "good morning"
+        elif 12 <= hours < 15:timenow = "good afternoog"
+        elif 15 <= hours < 18:timenow = "good evening"
+        else:timenow = "good night"
+        return timenow
